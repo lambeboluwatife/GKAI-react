08db4fa4efa2fd30b4e19911101922f69fbfdfa7
lambeboluwatife <boluwatifelambe@gmail.com>
2023-03-24 14:03:32 +0100

    a modal pops up when user wins the game

---
+++ src/components/Reload.jsx
@@ -0,0 +1,8 @@
+const Reload = () => {
+  //   const reload = () => {
+  //     location.reload();
+  //   };
+  return <div className="btn btn-success btn-block">Replay</div>;
+};
+
+export default Reload;
